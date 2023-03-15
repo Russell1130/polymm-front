50cb881b7853a767da3e8439edb90b6fd8c1ca4f
Russell1130 <devlord0625@gmail.com>
2023-03-15 09:09:41 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Vault from "./pages/Vault";
+import Header from "./components/Header"
+import Footer from "./components/Footer";
+import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <ThemeProvider>
+          <BrowserRouter>
+            <Routes >
+              <Route path="/" element={
+                <>
+                <Header />
+                <Home />
+                <Footer />
+                </>
+                } />
+              <Route path="/vault" element={<Vault />} />
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
+    );
 }
 
 export default App;
